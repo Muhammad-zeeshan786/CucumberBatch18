aeda95c43b93071c3a623191634a4a00be8eeaa6
Muhammad-zeeshan786 <xeeshanmuhammad13@gmail.com>
2024-02-01 01:26:00 +0000

    1

---
+++ src/test/java/runners/RunnerClass.java
@@ -0,0 +1,24 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//We need run with options to run with junit
+@RunWith(Cucumber.class)
+//cucumber options decide what to execute, where to execute
+@CucumberOptions(
+        //features we use to provide the path of the feature file
+        features = "src/test/resources/Features",
+        //glue below will link the above to the below
+        glue = "steps",
+        // dry run stops the actual execution when its TRUE - you have to change to FALSE then run the execution
+        dryRun = false,
+        tags ="@sprint3"
+
+
+)
+
+public class RunnerClass {
+
+}
---
+++ src/test/resources/Features/Login.feature
@@ -0,0 +1,8 @@
+Feature: Login functionality
+
+  //@sprint1
+  Scenario: Valid admin login
+    Given user is navigated to HRMS application
+    When user enters admin username and password
+    And user clicks on login button
+    Then user is successfully logged in
